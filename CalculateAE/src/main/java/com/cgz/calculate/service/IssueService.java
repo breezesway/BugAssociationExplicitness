d34c250968b3ed529d777b21c9160ab5334e56ce
breezesway <1048662534@qq.com>
2023-04-25 09:51:03 +0800

    检查该issue是否reopened过

--- CalculateAE/src/main/java/com/cgz/calculate/service/IssueService.java
+++ CalculateAE/src/main/java/com/cgz/calculate/service/IssueService.java
@@ -1,4 +1,28 @@
 package com.cgz.calculate.service;
 
+import com.cgz.calculate.dao.IssueDao;
+import com.cgz.calculate.model.Transition;
+
+import java.util.List;
+
 public class IssueService {
+
+    IssueDao issueDao = new IssueDao();
+
+    /**
+     * 检查该issue是否reopened过
+     */
+    public boolean hasReopened(String key){
+        List<Transition> transitions = issueDao.getTransitions(key);
+        for (Transition transition : transitions) {
+            if(transition.getFromString().equalsIgnoreCase("REOPENED") ||
+            transition.getToString().equalsIgnoreCase("REOPENED")){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
 }
